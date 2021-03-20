bd6b2db91af64f68561acf960d10e7cf0c961799
Pushpa <gautampushpa2008@gmail.com>
2021-03-20 01:41:07 -0400

    Added changes for evaluation of model and draft of report

--- Code/main.py
+++ Code/main.py
@@ -7,11 +7,44 @@ import numpy as np
 from pathlib import Path
 from datetime import datetime
 import constants
+from sklearn.metrics import confusion_matrix
 
 classes=[constants.Masked,constants.UnMasked,constants.NonPerson]
 
 DATA_REBUILD=False
 
+
+def generateConfusionMatrix(test_data,test_prediction):
+    print("Confusion Matrix:\n", confusion_matrix(test_data, test_prediction))
+
+def generateAccuracyResult(test_data,cnn):
+    correct = 0
+    total = 0
+    test_data_size =len(test_data)
+    with torch.no_grad():
+        for images, labels in test_data:
+            outputs = cnn(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    accuracy = 100 * (correct / total)
+    return accuracy
+    
+def generatePrecisionResult(test_data,test_prediction):
+    from sklearn.metrics import precision_score
+    precision = precision_score(test_data, test_prediction, average=None)
+    return precision
+    
+def generateRecallResult(test_data,test_prediction):
+    from sklearn.metrics import recall_score
+    recall = recall_score(test_data, test_prediction, average=None)
+    return recall
+
+def generateF1MeasureResult(precision,recall):
+    f1measure = 2 * (precision * recall) / (precision + recall)
+    return f1measure
+
+    
 class ImageClassifier():
     ImageDimensions=constants.imageSize
     trained_Data=[]
@@ -48,10 +81,11 @@ if DATA_REBUILD:
     batchData=ImageClassifier()
     batchData.ImageDataSetPrep()
 else:
-    files = glob.glob(constants.training_dataSavedPath+'*')
+    val = os.chdir(constants.training_dataSavedPath)
+    files = glob.glob(os.getcwd()+'/*')
+    #files = glob.glob(constants.training_dataSavedPath+'*')
     latest_file = max(files, key=os.path.getctime)
     print(latest_file)
-        
         
     
     
